370227623b1e7c88cf5e3a2d69d6ddeb5e857338
Aashirbad Karmacharya <aashirbadkarmacharya2929@gmail.com>
2025-05-02 13:27:25 +0545

    project completed

--- Hayasugiru/server/server.js
+++ Hayasugiru/server/server.js
@@ -20,7 +20,7 @@ await connectCloudinary()
 
 
 //allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://gorcbuys.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
