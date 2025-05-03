84936f59e7e3aa6b0103b08e69363fa07e85fa37
Aashirbad Karmacharya <aashirbadkarmacharya2929@gmail.com>
2025-05-03 17:04:50 +0545

    error solved

--- Hayasugiru/server/server.js
+++ Hayasugiru/server/server.js
@@ -12,42 +12,33 @@ import addressRouter from './routes/addressRoute.js';
 import orderRouter from './routes/orderRoute.js';
 import { stripeWebhooks } from './controllers/orderController.js';
 
-const app = express();
+const app = express()
 const port = process.env.PORT || 4000;
 
-await connectDB();
-await connectCloudinary();
+await connectDB()
+await connectCloudinary()
 
-const allowedOrigins = ['http://localhost:5173', 'https://gorcbuys.vercel.app'];
 
-// Stripe Webhook (must come before express.json)
-app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks);
+//allow multiple origins
+const allowedOrigins = ['http://localhost:5173', 'https://gorcbuys.vercel.app']
 
-// Middleware
+app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
+
+//Middleware configuration
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({origin: allowedOrigins, credentials: true}))
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like curl or mobile apps)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
+app.get('/', (req,res)=> res.send("API is Working"));
+app.use('/api/user', userRouter)
+app.use('/api/seller', sellerRouter)
+app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/address', addressRouter)
+app.use('/api/order', orderRouter)
 
-// Routes
-app.get('/', (req, res) => res.send("API is Working"));
-app.use('/api/user', userRouter);
-app.use('/api/seller', sellerRouter);
-app.use('/api/product', productRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/address', addressRouter);
-app.use('/api/order', orderRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+
+app.listen(port, ()=>{
+    console.log(`Server is running on http://localhost:${port}`)
+})
